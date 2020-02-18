aeba416787290bffe2ed5c9f2cd155c5dd7a9990
hl <215364491@qq.com>
2020-02-18 16:24:01 +0800

    environment build success

--- src/main.js
+++ src/main.js
@@ -8,9 +8,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-	// 1.普通用法
-  // render: function (h) { return h('h1',['hello',h('button',['按钮'])] )}
-	// 2.传组件对象(下方传App即是 当然这里没有具体写cpn)
-	// render: function(h){ return cpn}
-	render: function(h){ return h(App)}
+  render: function(h){ return h(App)}
 }).$mount('#app')
